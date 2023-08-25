a7ae81074600b02230fbad0cc21ef424fcecc5ec
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-08-25 11:53:05 +0200

    Update Attributes Sonda class

--- src/main/java/fireDetector/Sonda.java
+++ src/main/java/fireDetector/Sonda.java
@@ -1,5 +1,24 @@
 package fireDetector;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Sonda {
+	private Long id;
+	private String latitudine;
+	private String longitudine;
+	private Integer smokeLevel = 0;
+	private CentroDiControllo centroControllo;
+	
+	public Sonda(Long id,String latitudine, String longitudine, Integer smokeLevel) {
+		this.id = id;
+		this.latitudine = latitudine;
+		this.longitudine = longitudine;
+		this.smokeLevel = smokeLevel;
+	}
 	
 }
