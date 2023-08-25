e7c5bae001f71b8e900642897aa98312a62c0b30
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-08-25 10:39:49 +0200

    Initial commit

---
+++ src/main/java/fireDetector/ProgettoSettimana07Application.java
@@ -0,0 +1,13 @@
+package fireDetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimana07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimana07Application.class, args);
+	}
+
+}
